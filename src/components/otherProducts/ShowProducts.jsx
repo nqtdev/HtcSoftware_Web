29bb45a82d8cb2bf1432dc50c6d9c7e2a7a5ace8
nqtien <nguyenquoctien2000@gmail.com>
2024-11-06 17:11:30 +0700

    Update: V1.2.0 changer image png to webp

--- src/components/otherProducts/ShowProducts.jsx
+++ src/components/otherProducts/ShowProducts.jsx
@@ -1,9 +1,9 @@
 // @ Thêm sản phẩm mới ở đây
 import product_sr from 'assets/image_products/link_product/check-id-sr.webp'
 import product_metaEye from 'assets/image_products/link_product/meta-eye.webp'
-import product_rt from 'assets/image_products/link_product/check-id-r301.png'
-import product_et from 'assets/image_products/link_product/check-id-et100.png'
-import product_hn212 from 'assets/image_products/link_product/check-id-202.png'
+import product_rt from 'assets/image_products/link_product/check-id-r301.webp'
+import product_et from 'assets/image_products/link_product/check-id-et100.webp'
+import product_hn212 from 'assets/image_products/link_product/check-id-202.webp'
 const ShowProducts = [
   {
     slideContent: product_metaEye,
